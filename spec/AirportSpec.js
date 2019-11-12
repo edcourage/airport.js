dbaf1b58ca8413f7e98ca6ceeedb039586e965fe
Ed Courage <edcourage@live.com>
2019-11-12 14:57:28 +0000

    airport function create

---
+++ spec/AirportSpec.js
@@ -0,0 +1,39 @@
+describe("Airport", function(){
+
+  var heathrow;
+
+  beforeEach(function() {
+    heathrow = new Airport()
+    plane = "big plane"
+});
+
+
+  describe("land", function(){
+
+    it("returns a plane thats landed", function() {
+      expect(heathrow.land(plane)).toEqual(plane)
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+  })
+
+
+
+})
