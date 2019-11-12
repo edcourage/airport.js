365228a2b0af8263d4a1750f63fc907af5ea658a
Ed Courage <edcourage@live.com>
2019-11-12 17:28:31 +0000

    spies

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,11 +1,16 @@
 describe("Airport", function(){
 
   var heathrow;
+  var planeReal;
 
   beforeEach(function() {
     heathrow = new Airport()
-    plane = "big plane"
+    // plane = jasmine.createSpy("bigPlane")
+    planeReal = new Plane()
+    plane = jasmine.createSpyObj("plane",['isFlying'])
 });
+
+
 
 
   describe("land", function(){
@@ -13,7 +18,24 @@ describe("Airport", function(){
     it("returns a plane thats landed", function() {
       expect(heathrow.land(plane)).toEqual(plane)
     })
+
+    it('returns true', function() {
+      var plane_2;
+      // spyOn(plane, 'plane').and.callFake(function(isFlying){return true})
+      spyOn(plane_2, 'isFlying').andReturn(true)
+
+      // spyOn(plane,'isFlying').and.returnValue(true);
+
+      expect(heathrow.spyTest(plane_2)).toEqual(true)
+
+
+    })
+
+
+
   })
+
+
 
   describe("take off", function(){
 
@@ -21,6 +43,8 @@ describe("Airport", function(){
       expect(heathrow.takeOff(plane)).toEqual(plane)
     })
   })
+
+
 
 
 
--- src/Airport.js
+++ src/Airport.js
@@ -1,10 +1,20 @@
-function Airport() {
+function Airport(capacity = 10) {
+  this.capacity = capacity
+  this.planes = []
   }
 
   Airport.prototype.land = function(plane) {
+    this.planes.push(plane)
     return plane;
   }
 
   Airport.prototype.takeOff = function(plane) {
+    var index = this.planes.indexOf(plane)
+    this.planes.splice(index,1)
     return plane;
+  }
+
+  Airport.prototype.spyTest = function(plane) {
+
+    return plane.isFlying()
   }
